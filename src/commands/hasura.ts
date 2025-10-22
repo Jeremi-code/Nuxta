bbac8e6f97e53bfd4a2e59b5a64c5bd5274c696d
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-10-22 23:57:27 +0300

    chore: hasura command

---
+++ src/commands/hasura.ts
@@ -0,0 +1,42 @@
+import { Command } from "commander";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { ensureProjectInitialized } from "../utils";
+import { createSpinner } from "../utils/spinner";
+
+export const hasuraCommand = new Command()
+  .name("hasura")
+  .description("Set up GraphQL with Hasura in the Nuxt.js project.")
+  .action(async () => {
+    const spinner = createSpinner("Setting up GraphQL with Hasura...");
+    try {
+      ensureProjectInitialized();
+      const answers = await inquirer.prompt([
+        {
+          type: "input",
+          name: "hasuraEndpoint",
+          message: "Enter your Hasura GraphQL endpoint:",
+          default: "http://localhost:8080/v1/graphql",
+        },
+        {
+          type: "input",
+          name: "hasuraAdminSecret",
+          message:
+            "Enter your Hasura admin secret (leave blank if not applicable):",
+        },
+      ]);
+
+      spinner.succeed(chalk.green("GraphQL with Hasura setup initiated."));
+      console.log(chalk.blue("Hasura configuration:"));
+      console.log(`Endpoint: ${answers.hasuraEndpoint}`);
+      console.log(
+        `Admin Secret: ${answers.hasuraAdminSecret ? "******" : "None"}`
+      );
+    } catch (error) {
+      spinner.fail(
+        chalk.red(
+          `Failed to set up GraphQL with Hasura: ${(error as Error).message}`
+        )
+      );
+    }
+  });
