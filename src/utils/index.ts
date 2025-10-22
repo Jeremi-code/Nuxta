51f024272000e5401f1b9bdae969cb6bf3c967d6
Ermias Sintayehu <ermiassintayehu13@gmail.com>
2025-10-22 23:54:20 +0300

    move utils to utils directory and rename it to index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,16 +2,18 @@ import { exec } from "child_process";
 import * as fs from "fs";
 import * as path from "path";
 import chalk from "chalk";
+import { createSpinner } from "./spinner";
 
 export function executeCommand(command: string, cwd?: string): Promise<string> {
   return new Promise((resolve, reject) => {
     exec(command, { cwd }, (error, stdout, stderr) => {
+      console.log(stdout);
       if (error) {
         reject(error);
         return;
       }
       if (stderr) {
-        console.error(`stderr: ${stderr}`);
+        createSpinner("").fail(`stderr: ${stderr}`);
       }
       resolve(stdout);
     });
@@ -32,11 +34,23 @@ export function writeToFile(filePath: string, content: string): Promise<void> {
 
 export function ensureProjectInitialized() {
   if (!fs.existsSync(path.join(path.join(process.cwd(), ".nuxt")))) {
-    console.error(
-      chalk.red(
-        "❌ No Nuxt project detected. Please run `create-nuxt-hasura-cli init <project-name>` first."
-      )
+    createSpinner("").fail(
+      "❌ No Nuxt project detected. Please run `create-nuxt-hasura-cli init <project-name>` first."
     );
     process.exit(1);
   }
+}
+
+export async function addScriptToPackageJson(scripts: Record<string, string>) {
+  const packageJsonPath = path.join(process.cwd(), "package.json");
+  if (!fs.existsSync(packageJsonPath)) {
+    createSpinner("").fail("❌ No package.json file found.");
+    process.exit(1);
+    return;
+  }
+
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+  packageJson.scripts = { ...packageJson.scripts, ...scripts };
+
+  await writeToFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
 }
