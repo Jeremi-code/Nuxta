bff100a6add2956ab8d364ea1105c9e229780350
jeremi-code <ermiassintayehu13@gmail.com>
2025-12-08 14:22:52 +0300

    feat: Display a colored ASCII logo when the CLI is run without arguments.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { Command } from "commander";
+import chalk from "chalk";
 import { initCommand } from "./commands/init";
 import { codegenCommand } from "./commands/codegen";
 import { getSchemaCommand } from "./commands/get-schema";
 import { graphqlClientCommand } from "./commands/graphql-client";
+
+const logo = `
+${chalk.hex("#00DC82")("        .d$b.")}
+${chalk.hex("#00DC82")("       i$$A$$L  .d$b         d$Pd$b")}
+${chalk.hex("#00DC82")("     .$$F` `$$L.$$A$$.           $$")}
+${chalk.hex("#00DC82")("    j$$'    `4$$:` `$$.      d$$$$$")}
+${chalk.hex("#00DC82")("   j$$'     .4$:    `$$.     $P   $")}
+${chalk.hex("#00DC82")("  j$$`     .$$:      `4$L    $P   $")}
+${chalk.hex("#00DC82")(" :$$:____.d$$:  _____.:$$:   d$$$$$")}
+${chalk.hex("#00DC82")(" `4$$$$$$$$P` .i$$$$$$$$P`")}
+`;
 
 export const program = new Command();
 
@@ -18,8 +30,11 @@ program.addCommand(codegenCommand);
 program.addCommand(getSchemaCommand);
 program.addCommand(graphqlClientCommand);
 
-program.parse(process.argv);
-
+// Show logo when running without arguments
 if (!process.argv.slice(2).length) {
+  console.log(logo);
   program.outputHelp();
+  process.exit(0);
 }
+
+program.parse(process.argv);
